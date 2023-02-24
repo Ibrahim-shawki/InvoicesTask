7fd8589fcb003e864ecc38c0ea187fb7b554f2d8
Ibrahim-shawki <hemasallam03@gmail.com>
2023-02-24 05:41:32 +0200

    redirect to Login if Not auth & Can't Redirect to Login if already Login

--- InvoicesTask/routes/web.php
+++ InvoicesTask/routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
