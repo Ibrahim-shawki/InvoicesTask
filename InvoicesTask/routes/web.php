8f1712d132dbb04f756f11e9a81a62ff9a26a768
Ibrahim-shawki <hemasallam03@gmail.com>
2023-02-24 05:30:53 +0200

    false register After Once register Admin

--- InvoicesTask/routes/web.php
+++ InvoicesTask/routes/web.php
@@ -17,10 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
